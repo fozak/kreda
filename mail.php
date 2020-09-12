0167c2fc823b71e9e7ec0d63384a1896dee3dfc0
Pradip Bhattarai <er.bpradip@gmail.com>
2020-09-12 08:14:30 +0545

    moidified mail.php file for some server stuff

--- mail.php
+++ mail.php
@@ -1,6 +1,26 @@
 <?php
     if(isset['$_POST']){
-        
-    }
+        if(isset($_POST['submit']) && $_POST['submit'] = 'submit'){
 
+        }else{
+            $message = "Form not Submitted";
+        }
+    }else{
+        $message = "Post Data not found";
+    }
+?>
+
+
+<?php 
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
 ?>
